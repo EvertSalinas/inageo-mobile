f91f10e00dd5b038e63794269c582f820c57eaab
evertsalinas <evertsalinas88@gmail.com>
2020-03-21 13:23:22 -0600

    lot of stuff

---
+++ src/api/config.js
@@ -0,0 +1,6 @@
+export const apiConfig = {
+    baseUrl: 'http://inageo-development.us-west-2.elasticbeanstalk.com/api/',
+    // should end with a slash
+    clientId: '1',
+    clientSecret: 'xc34jamesDevV41XwKbWhrsGgHvR3hjwG8',
+};
--- src/screens/auth/LoginScreen.js
+++ src/screens/auth/LoginScreen.js
@@ -1,10 +1,17 @@
 import React, { Component, useEffect } from 'react';
 import { View, Text, StyleSheet, Button, TextInput } from 'react-native';
-import FBLoginButton from '../../components/FBLoginButton'
-import LoginForm from '../../components/LoginForm'
+import FBLoginButton from '../../components/FBLoginButton';
+import LoginForm from '../../components/LoginForm';
+import GoogleSignInComponent from '../../components/GoogleSignInComponent';
+import axios from 'axios';
 
 export default class LoginScreen extends Component {
     render() {
+        // axios.get('http://inageo-development.us-west-2.elasticbeanstalk.com/api/token/')
+        axios.get('https://dog.ceo/api/breeds/image/random')
+            .then(response => console.log(response.data))
+            .catch(err => console.log(err))
+
         return (
             <View style={styles.container}>
                 <View style={styles.formContainer}>
@@ -15,6 +22,7 @@ export default class LoginScreen extends Component {
                         <FBLoginButton />
                     </View>
                     <Button title="Home Screen" onPress={() => this.props.navigation.navigate('Home')} />
+                    <GoogleSignInComponent />
                 </View>
             </View>
         );
